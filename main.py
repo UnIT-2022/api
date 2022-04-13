92b3bf427601070887e0dc41eb71bc61682189f2
nohavond <nohavond@fit.cvut.cz>
2022-04-13 10:53:07 +0200

    PARSER

---
+++ main.py
@@ -0,0 +1,29 @@
+import json
+import os
+import pathlib
+
+import xmltodict as xml
+
+path = "/home/ondrej/soutez/PG24A"
+
+
+def read_xml():
+    json_list = []
+    for filename in os.listdir(path):
+        if not filename.endswith('.xml'):
+            continue
+        name = os.path.join(path, filename)
+        with open(name) as xml_file:
+            data_dict = xml.parse(xml_file.read())
+            xml_file.close()
+            json_data = json.dumps(data_dict, indent=2)
+        json_list.append(json_data)
+    return json_list
+
+
+def main():
+    read_xml()
+
+
+if __name__ == "__main__":
+    main()
